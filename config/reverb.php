64c5474d540674eb1f5b56c4273fb33f4e13402f
Mizanur Rahman Khan <engr.mrksohag@gmail.com>
2025-01-26 02:50:39 +0600

    update

--- config/reverb.php
+++ config/reverb.php
@@ -33,7 +33,9 @@ return [
             'port' => env('REVERB_SERVER_PORT', 8080),
             'hostname' => env('REVERB_HOST'),
             'options' => [
-                'tls' => [],
+                'tls' => [
+                    'verify_peer' => env('REVERB_TLS_VERIFY_PEER', false),
+                ],
             ],
             'max_request_size' => env('REVERB_MAX_REQUEST_SIZE', 10_000),
             'scaling' => [
